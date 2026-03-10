b21cc793e82cd4b54fc0eb12c2b0037980e0763d
nikashatheepic <nikasha.datiashvili@gmail.com>
2026-03-10 15:41:43 +0400

    Update cards.py

--- cards.py
+++ cards.py
@@ -1,5 +1,5 @@
 from engine import draw_card
-
+import random
 
 class Card:
     def __init__(self, name, cost, base_power, type, ability, priority):
@@ -11,14 +11,15 @@ class Card:
         self.priority = priority
         self.ongoing_power = 0
         self.owner = None
+        self.order = None
 
 def iron_man_ability(player, location):
     if player.number == 1:
-        location.ongoing_power1 += location.base_power1
+        location.ongoing_power1 += location.base_power1 + location.ongoing_power1
     else:
-        location.ongoing_power2 += location.base_power2
+        location.ongoing_power2 += location.base_power2 + location.ongoing_power2
 
-def forge_ability(player, game_locations, location_index):
+def forge_ability(player, game_locations, location_index, player1, player2, card):
     player.pending_buffs += 2
 
 def adam_warlock_ability(card, player, game_locations, location_index):
@@ -29,22 +30,45 @@ def adam_warlock_ability(card, player, game_locations, location_index):
 
     if winning:
         draw_card(player)
-        print ("BORN AGAIN!")
     else:
         card.base_power += 1
-        if player.number == 1:
-            game_locations[location_index].base_power1 += 1
-        else:
-            game_locations[location_index].base_power2 += 1
-        print ("BORN AGAIN!")
+
+def silver_sable_ability(player, game_locations, location_index, player1, player2, card):
+    if player.number == 1:
+        if len(player2.deck) > 0:
+            player2.deck[0].base_power -= 2
+            card.base_power += 2
+    else:
+        if len(player1.deck) > 0:
+            player1.deck[0].base_power -= 2
+            card.base_power += 2
+
+def iceman_ability(player, game_locations, location_index, player1, player2, card):
+    if player.number == 1:
+        if len(player2.hand) > 0:
+            target = random.randint(0,len(player2.hand)-1)
+            player2.hand[target].cost += 1
+    else:
+        if len(player1.hand) > 0:
+            target = random.randint(0,len(player1.hand)-1)
+            player1.hand[target].cost += 1
+
+def gambit_ability(player, game_locations, location_index, player1, player2, card):
+    pass
 
 
 iron_man = Card('iron man', 5, 0, "ongoing", iron_man_ability, 1)
 forge = Card('forge', 2, 2, "on_reveal", forge_ability, 0)
 adam_warlock = Card('adam warlock', 2, 0, "end_of_turn", adam_warlock_ability, 0)
+silver_sable = Card('silver sable', 1, 0, "on_reveal", silver_sable_ability, 0)
+iceman = Card('iceman', 1, 2, "on_reveal", iceman_ability, 0)
+gambit = Card('gambit',3,3,'on_reveal',gambit_ability,0)
 
 card_lookup = {
     "iron man" : iron_man,
     "forge" : forge,
-    "adam warlock" : adam_warlock
+    "adam warlock" : adam_warlock,
+    "silver sable" : silver_sable,
+    "iceman" : iceman,
+    "gambit" : gambit
 }
