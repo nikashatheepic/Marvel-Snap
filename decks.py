b878ea7760c70048660806363e2bea5290ce6b3d
nikashatheepic <nikasha.datiashvili@gmail.com>
2026-03-10 15:40:58 +0400

    Update decks.py

--- decks.py
+++ decks.py
@@ -1,4 +1,4 @@
 from cards import *
 
-player1_deck = [iron_man.name,forge.name,adam_warlock.name,4,5,6,7,8,9,10,11,12]
-player2_deck = [iron_man.name,forge.name,adam_warlock.name,4,5,6,7,8,9,10,11,12]
+player1_deck = [iron_man,forge,adam_warlock,silver_sable,iceman,gambit]
+player2_deck = [iron_man,forge,adam_warlock,silver_sable,iceman,gambit]
